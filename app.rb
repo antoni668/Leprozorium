1185c5c0c07e8936bfd2bb4263d568f28f96b677
antoni668 <antoni668@mail.ru>
2022-03-30 14:31:17 +0600

    Add stab for /newpost

--- app.rb
+++ app.rb
@@ -7,3 +7,7 @@ require 'sqlite3'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/newpost' do
+	erb "Hello World"
+ end
